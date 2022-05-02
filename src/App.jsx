9673c86bc1c085386fae13d890c3ab9ff1ea8f87
Vishnu642 <dkvishnuvardhan94@gmail.com>
2022-05-02 15:22:03 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+import { Logout } from "./components/Logout";
+import { NewOrder } from "./components/NewOrder";
+import { Orders } from "./components/Orders";
+
+import {Link} from "react-router-dom"
+import {Routes,Route} from "react-router-dom"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <Link className="nav-home" to="/">
+          Home
+        </Link>
+      
+        {/* Show either login or logout below */}
+        {/* <Link className="nav-logout" to="/logout">
+          Logout
+        </Link> */}
+        <Link className="nav-login" to="/login">
+          Login
+        </Link>
+      </div>
+
+      <Routes>
+        {/* Routes are as follows:
+        Route      Component
+        /           Home
+        /login      Login
+        /logout     Logout
+        /orders     Orders    Protected
+        /neworder   NewOrder  Protected
+        */}
+        <Route path="/" element={<Home/>} ></Route>
+        <Route path="/login" element={<Login/>} ></Route>
+        <Route path="/logout" element={<Logout/>} ></Route>
+        <Route path="/orders" element={<Orders/>} ></Route>
+        <Route path="/neworder" element={<NewOrder/>} ></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Login.jsx
@@ -0,0 +1,25 @@
+
+import {Navigate, useNavigate,Link} from "react-router-dom"
+export const Login = () => {
+  return (
+    <div>
+      <input
+        className="username"
+        type="text"
+        name="username"
+        placeholder="Enter Username"
+      />
+      <input
+        className="password"
+        type="password"
+        name="password"
+        placeholder="Enter password"
+      />
+      {/* On this button click make network req to find user with same username and password */}
+      {/* get his role, if role is `admin` take him to `/orders` page otherwise take him to `/neworder` */}
+      <Link to="/neworder" ><button
+       
+    className="submit">Login</button></Link>
+    </div>
+  );
+};
